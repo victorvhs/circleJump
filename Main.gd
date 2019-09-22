14c24e1f61a9c80f3fab080dea08c0721662a317
Victor Reis <victor.h.s.reis@gmail.com>
2019-09-21 23:00:56 -0300

    fim aula 2

--- Main.gd
+++ Main.gd
@@ -15,5 +15,18 @@ func new_game():
 	player.position = $StartPosition.position
 	add_child(player)
 	player.connect("captured",self, "_on_Jumper_captured")
+	spawn_circle($StartPosition.position)
 	
-	
+func spawn_circle(_position = null):
+	var c = Circle.instance()
+	if !_position:
+		var x = rand_range(-150,150)
+		var y = rand_range(-500,-400)
+		_position = player.target.position + Vector2(x,y)
+	add_child(c)
+	c.init(_position)
+		
+func _on_Jumper_captured(object):
+	$Camera2D.position = object.position
+	object.capture()
+	call_deferred("spawn_circle")
